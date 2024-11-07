16fa6cfb783943ddd4f89bd2004d1c3893f71a9e
sanafancy <xutingting197@gmail.com>
2024-11-07 10:32:32 +0100

    archivos creados

---
+++ src/main/java/com/example/libreria/dominio/entidades/Greeting.java
@@ -0,0 +1,18 @@
+package com.example.libreria.dominio.entidades;
+
+public class Greeting {
+    private String person;
+    private String content;
+    public String getPerson() {
+        return person;
+    }
+    public void setPerson(String person) {
+        this.person = person;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
