8a42badb71258ebd9aa62ac9dd19add082a1b989
sanafancy <xutingting197@gmail.com>
2024-11-08 14:25:00 +0100

    Modificación de las clase Greeting, GreetingController y GreetingDAO y cosas para el bbdd

--- src/main/java/com/example/libreria/dominio/gestores/GreetingController.java
+++ src/main/java/com/example/libreria/dominio/gestores/GreetingController.java
@@ -1,21 +1,25 @@
 package com.example.libreria.dominio.gestores;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import com.example.libreria.dominio.entidades.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import es.uclm.library.persistency.GreetingDAO;
 @Controller
 public class GreetingController {
+    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+    @Autowired
+    private GreetingDAO greetingDAO;
     @GetMapping("/greeting")
     public String greetingForm(Model model) {
         model.addAttribute("greeting", new Greeting());
+        log.info(greetingDAO.findAll().toString());
         return "greeting";
     }
     @PostMapping("/greeting")
     public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
         model.addAttribute("greeting", greeting);
+        Greeting savedGreeting = greetingDAO.save(greeting);
+        log.info("Saved greeting: " + savedGreeting);
         return "result";
     }
 }
