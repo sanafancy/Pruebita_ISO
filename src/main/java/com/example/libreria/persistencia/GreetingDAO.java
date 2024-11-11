69c3bc8cb3df8d5cad569c3f7cdeedf5f65175b1
sanafancy <xutingting197@gmail.com>
2024-11-11 12:34:13 +0100

    Cambios correctos de la p3

---
+++ src/main/java/com/example/libreria/persistencia/GreetingDAO.java
@@ -0,0 +1,21 @@
+package com.example.libreria.persistencia;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.libreria.dominio.entidades.*;
+@Repository
+public interface GreetingDAO extends JpaRepository<Greeting, Long> {
+/*
+* Se hereda de JpaRepository operaciones de acceso a datos comunes a todas las
+entidades:
+* E save(E)
+* List<E> saveAll(List<E>)
+* List<E> findAll()
+* E findById(id)
+* delete(E)
+* deleteById(id)
+* ...
+*
+* Aquí se podrían definir consultas de selección más específicas y complejas
+**/
+}
