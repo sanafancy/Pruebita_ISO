8b2b246440f955f3b2de3b641b3df285f775b95e
sanafancy <xutingting197@gmail.com>
2024-11-11 09:19:50 +0100

    Modificación de las clase Greeting, GreetingController y GreetingDAO y cosas para el bbdd

--- src/main/java/com/example/libreria/dominio/entidades/Greeting.java
+++ src/main/java/com/example/libreria/dominio/entidades/Greeting.java
@@ -14,40 +14,31 @@ public class Greeting {
     private String person;
     @Column
     private String content;
-
     public Greeting() {
     }
-
     public Greeting(String person, String content) {
         super();
         this.person = person;
         this.content = content;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getPerson() {
         return person;
     }
-
     public void setPerson(String person) {
         this.person = person;
     }
-
     public String getContent() {
         return content;
     }
-
     public void setContent(String content) {
         this.content = content;
     }
-
     @Override
     public String toString() {
         return String.format("Greeting [id=%s, person=%s, content=%s]", id, person, content);
